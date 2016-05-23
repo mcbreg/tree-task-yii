bb7086cb7d828dac2d9d3581990df9bca1e733bc
mcbreg <mcbreg@gmail.com>
2016-05-23 11:32:32 +0300

    db pass

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '4a5vo7s2',
+    'password' => 'demo',
     'charset' => 'utf8',
 ];
